a0883848e1e5e40d64590dab58ac977a4cda2cce
Fritsefreek <rvremco@hotmail.com>
2016-09-19 23:33:36 +0200

    Add project files.

---
+++ SimpleORM/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SimpleORM
+{
+    class Database
+    {
+        protected static MySqlConnection connect()
+        { 
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "root";
+            builder.Database = "world";
+
+            MySqlConnection connection = new MySqlConnection(builder.ToString());
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
---
+++ SimpleORM/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SimpleORM
+{
+    class Program : Database
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection connection = Database.connect();
+            MySqlCommand cities = new MySqlCommand("SELECT * FROM city", connection);
+            MySqlDataReader citiesRDR = cities.ExecuteReader();
+
+            while (citiesRDR.Read())
+            {
+                Console.WriteLine(citiesRDR.GetString(1));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
